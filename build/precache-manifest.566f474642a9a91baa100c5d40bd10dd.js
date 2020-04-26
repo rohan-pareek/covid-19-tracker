f836e4b2e3861f610619869a9b73fc42a10ff699
Rohan Pareek <rohaniscoding@gmail.com>
2020-04-26 14:52:44 +0530

    adding build folder

---
+++ build/precache-manifest.566f474642a9a91baa100c5d40bd10dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe0f5d4b144649f40ef026fa99d9614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47e1d17c9b513202956f",
+    "url": "/static/css/main.b16cb716.chunk.css"
+  },
+  {
+    "revision": "53a2e01484626d1c7d15",
+    "url": "/static/js/2.3a8deac6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3a8deac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e1d17c9b513202956f",
+    "url": "/static/js/main.49c2f79a.chunk.js"
+  },
+  {
+    "revision": "458b4f96ab9c97c67cef",
+    "url": "/static/js/runtime-main.36994c0b.js"
+  },
+  {
+    "revision": "e0f3ae4744ff5c26b2021be82364c104",
+    "url": "/static/media/corona_logo.e0f3ae47.png"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/static/media/covid19_logo.6ce9074d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
